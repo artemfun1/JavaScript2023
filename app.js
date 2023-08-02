b2cee059d89c24cdbf76c728e8dbd43402ead4af
ArtemPeshkov <a.o.peshkov@yandex.ru>
2023-08-02 15:24:56 +0500

    lern js

---
+++ app.js
@@ -0,0 +1,42 @@
+"use strict";
+
+
+// let num = 42
+
+// let firstName = 'Aaaa'
+
+// num = 10
+
+// const isProgrammer = true 
+
+// let a = 0;
+// console.log( Boolean(a) ); 
+
+// let b = "0";
+// console.log( Boolean(b) ); 
+
+// console.log(a)
+// console.log(typeof(a))
+// console.log(b)
+// console.log(typeof(b))
+
+// console.log(` budet ${counter}`);
+
+
+const resultElement = document.getElementById('result')
+const input1 = document.getElementById('input1')
+const input2 = document.getElementById('input2')
+
+
+console.log(input1.value)
+
+
+// console.log(resultElement.textContent)
+
+// resultElement.textContent = 40
+
+// console.log(resultElement.textContent)
+
+
+
+
