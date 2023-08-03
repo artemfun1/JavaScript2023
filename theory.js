90c5af28589e72e7611630e1f98482fe896b8562
ArtemPeshkov <a.o.peshkov@yandex.ru>
2023-08-03 14:08:58 +0500

    some task from learn.java

---
+++ theory.js
@@ -0,0 +1,59 @@
+// switch (browser) {
+//   case 'Edge':
+//     alert( "You've got the Edge!" );
+//     break;
+
+//   case 'Chrome':
+//   case 'Firefox':
+//   case 'Safari':
+//   case 'Opera':
+//     alert( 'Okay we support these browsers too' );
+//     break;
+
+//   default:
+//     alert( 'We hope that this page looks ok!' );
+// }
+
+ 
+// if (browser === 'Edge') {
+//   alert( "You've got the Edge!" );
+// } 
+// else if ((browser === 'Chrome') || (browser ==='Firefox')){
+//   alert( 'Okay we support these browsers too' );
+    
+// } else alert( 'We hope that this page looks ok!' )
+
+
+
+
+const number = +prompt('Введите число между 0 и 3', '');
+
+switch (number) {
+  case 0: 
+  alert('Вы ввели число 0');
+  break;
+  case 1: 
+  alert('Вы ввели число 1');
+  break;
+  case 2: 
+  case 3: 
+  alert('Вы ввели число 2, а может и 3');
+  break;
+
+}
+
+
+
+
+
+
+
+// console.log( typeof 'Hello')
+// console.log( typeof 10)
+// console.log( typeof {})
+// console.log( typeof null)
+// console.log( typeof undefined)
+// console.log( typeof true)
+// console.log( typeof [])
+// console.log( typeof (() => {}))
+
