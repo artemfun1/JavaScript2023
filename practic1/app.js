4de76c7c8fc2ffabbddc192aef03f4f0fcf8ae39
ArtemPeshkov <a.o.peshkov@yandex.ru>
2023-08-06 20:11:58 +0500

    lernArrayAndObject

--- practic1/app.js
+++ practic1/app.js
@@ -24,49 +24,49 @@
 // console.log(` budet ${counter}`);
 
 
-const resultElement = document.getElementById('result')
-const input1 = document.getElementById('input1')
-const input2 = document.getElementById('input2')
-const submitBtn = document.getElementById('submit')
-const plusBtn = document.getElementById('plus')
-const minusBtn = document.getElementById('minus')
+// const resultElement = document.getElementById('result')
+// const input1 = document.getElementById('input1')
+// const input2 = document.getElementById('input2')
+// const submitBtn = document.getElementById('submit')
+// const plusBtn = document.getElementById('plus')
+// const minusBtn = document.getElementById('minus')
 
-let action ='+'
-
-
-plusBtn.onclick = function(){
-  action ='+'
-}
-
-minusBtn.onclick = function(){
-  action = '-'
-}
-
-function printResult (result){
-  if (result < 0){
-      resultElement.style.color = 'red'
-    } else {
-      resultElement.style.color = 'green'
-    }
-    resultElement.textContent = result
-}
-
-function computeNumbersWithAction (inp1, inp2, actionSymbol) {
-  const num1 = Number(inp1.value) 
-  const num2 = Number(inp2.value) 
-  if (actionSymbol == '+'){
-      return num1 + num2
-  } 
-  if (actionSymbol == '-'){
-    return num1 - num2
-}
-}
+// let action ='+'
 
 
-submitBtn.onclick = function (){
+// plusBtn.onclick = function(){
+//   action ='+'
+// }
 
-  const result = computeNumbersWithAction (input1, input2, action)
-  printResult (result)
+// minusBtn.onclick = function(){
+//   action = '-'
+// }
+
+// function printResult (result){
+//   if (result < 0){
+//       resultElement.style.color = 'red'
+//     } else {
+//       resultElement.style.color = 'green'
+//     }
+//     resultElement.textContent = result
+// }
+
+// function computeNumbersWithAction (inp1, inp2, actionSymbol) {
+//   const num1 = Number(inp1.value) 
+//   const num2 = Number(inp2.value) 
+//   if (actionSymbol == '+'){
+//       return num1 + num2
+//   } 
+//   if (actionSymbol == '-'){
+//     return num1 - num2
+// }
+// }
+
+
+// submitBtn.onclick = function (){
+
+//   const result = computeNumbersWithAction (input1, input2, action)
+//   printResult (result)
 
 
 
@@ -77,4 +77,4 @@ submitBtn.onclick = function (){
   //   const sum = Number(input1.value) - Number(input2.value)
   //   printResult(sum)
   // }
-}
+// }
---
+++ practic2/app2.js
@@ -0,0 +1,171 @@
+/* Theory
+// const array = [1, 2, 3, 5, 20, 111]
+// const arrayString = [ 'a', 'b', 'c', null, 12]
+// const array = new Array(1, 2, 3, 5, 20 ,42)
+// console.log(array.length)
+// console.log(array[1])
+// console.log(array[array.length - 1])
+// array[0] = 'Privet!'
+// console.log(array)
+// array[array.length] = 'becon'
+// console.log(array) */
+
+
+const inputElement = document.getElementById('title');
+let createButton = document.getElementById('create');
+let listElement = document.getElementById('list');
+
+// console.log(listElement.length)
+
+// const notes = ['записать блок про массивы', 'рассказать теорию обьектов',]
+
+
+
+// createButton.onclick = function (){
+//   if (inputElement.value.length === 0){
+//     return;
+//   }
+  
+
+// listElement.insertAdjacentHTML('beforeend', getNoteTemplate(inputElement.value)
+// )
+// inputElement.value = '';
+// }
+
+// function getNoteTemplate(title) {
+//   return 
+//   `
+//     <li
+//       class="list-group-item d-flex justify-content-between align-items-center"
+//       >
+//       <span>${title}</span>
+//       <span>
+//         <span class="btn btn-small btn-success">&check;</span>
+//         <span class="btn btn-small btn-danger">&times;</span>
+//       </span>
+//     </li>
+//   `
+// }
+
+/* Object theory 
+
+// const person = {
+
+//   firstName: 'Artem',
+//   lastName: 'Artem2',
+//   year: 1993, 
+//   hisGirlfriend: false,
+//   languages: ['ru', 'en'],
+//   getFullName: function(){
+//     console.log(person.firstName + ' ' + person.lastName)
+//   }
+// }
+// console.log(person.getFullName())
+// console.log(person.languages)
+// console.log(person['languages'])
+// const key = 'hisGirlfriend'
+
+// console.log(person[key])
+
+// person.hisGirlfriend = true
+
+// console.log(person[key])
+// console.log()
+
+*/
+
+
+const notes = [
+  {
+    title: 'записать блок про массивы',
+    complited: false,
+  },
+  {
+    title: 'рассказать теорию обьектов',
+    complited: true,
+  },
+  
+]
+
+function render (){
+  listElement.innerHTML = '';
+  if(notes.length === 0) {
+    listElement.innerHTML = '<p>Нет элементов</p>'
+  }
+  for (let i=0; i < notes.length; i++){
+  listElement.insertAdjacentHTML('beforeend', getNoteTemplate(notes[i], i))
+  }
+  // for (let note of notes){
+  //   listElement.insertAdjacentHTML('beforeend', getNoteTemplate(note))
+  // }
+}
+
+render();
+
+
+createButton.onclick = function (){
+  if (inputElement.value.length === 0){
+    return;
+  }
+  
+const newNote = {
+    title: inputElement.value,
+    complited: false,
+};
+
+notes.push(newNote);
+render();
+inputElement.value = '';
+};
+
+listElement.onclick = function (event){
+  if (event.target.dataset.index){
+    const index = parseInt(event.target.dataset.index)
+    const type = event.target.dataset.type
+     
+    if(type === "toogle"){
+      notes[index].complited = !notes[index].complited
+
+     
+    } else if (type === "remove"){
+      notes.splice(index, 1)
+
+
+    }
+    render();
+
+  }
+  
+  // console.log((event.target.dataset.index), (event.target.dataset.type) )
+};
+
+function getNoteTemplate(note, index) {
+  return `
+    <li
+      class="list-group-item d-flex justify-content-between align-items-center"
+      >
+        <span 
+        class="${note.complited ? 'text-decoration-line-through' : '' }">${note.title}
+        </span>
+      <span>
+          <span 
+          class="btn btn-small 
+          btn-${note.complited ? 'warning' : 'success'}" 
+          data-index = "${index}" 
+          data-type="toogle"
+          >&check;
+          </span>
+
+          <span 
+          class="btn btn-small btn-danger"
+          data-index = "${index}" 
+          data-type="remove"
+          >&times;
+          </span>
+      </span>
+    </li>
+  `
+}
+
+
+
