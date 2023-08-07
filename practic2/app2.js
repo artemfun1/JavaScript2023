cc6b0863ba699f5c8368d08ef0bbaa1e64c9981a
ArtemPeshkov <a.o.peshkov@yandex.ru>
2023-08-07 21:46:46 +0500

    some lern theory array

--- practic2/app2.js
+++ practic2/app2.js
@@ -11,9 +11,9 @@
 // console.log(array) */
 
 
-const inputElement = document.getElementById('title');
-let createButton = document.getElementById('create');
-let listElement = document.getElementById('list');
+// const inputElement = document.getElementById('title');
+// let createButton = document.getElementById('create');
+// let listElement = document.getElementById('list');
 
 // console.log(listElement.length)
 
@@ -75,97 +75,97 @@ let listElement = document.getElementById('list');
 */
 
 
-const notes = [
-  {
-    title: 'записать блок про массивы',
-    complited: false,
-  },
-  {
-    title: 'рассказать теорию обьектов',
-    complited: true,
-  },
+// const notes = [
+//   {
+//     title: 'записать блок про массивы',
+//     complited: false,
+//   },
+//   {
+//     title: 'рассказать теорию обьектов',
+//     complited: true,
+//   },
   
-]
+// ]
 
-function render (){
-  listElement.innerHTML = '';
-  if(notes.length === 0) {
-    listElement.innerHTML = '<p>Нет элементов</p>'
-  }
-  for (let i=0; i < notes.length; i++){
-  listElement.insertAdjacentHTML('beforeend', getNoteTemplate(notes[i], i))
-  }
+// function render (){
+//   listElement.innerHTML = '';
+//   if(notes.length === 0) {
+//     listElement.innerHTML = '<p>Нет элементов</p>'
+//   }
+//   for (let i=0; i < notes.length; i++){
+//   listElement.insertAdjacentHTML('beforeend', getNoteTemplate(notes[i], i))
+//   }
   // for (let note of notes){
   //   listElement.insertAdjacentHTML('beforeend', getNoteTemplate(note))
   // }
-}
+// }
 
-render();
+// render();
 
 
-createButton.onclick = function (){
-  if (inputElement.value.length === 0){
-    return;
-  }
+// createButton.onclick = function (){
+//   if (inputElement.value.length === 0){
+//     return;
+//   }
   
-const newNote = {
-    title: inputElement.value,
-    complited: false,
-};
+// const newNote = {
+//     title: inputElement.value,
+//     complited: false,
+// };
 
-notes.push(newNote);
-render();
-inputElement.value = '';
-};
+// notes.push(newNote);
+// render();
+// inputElement.value = '';
+// };
 
-listElement.onclick = function (event){
-  if (event.target.dataset.index){
-    const index = parseInt(event.target.dataset.index)
-    const type = event.target.dataset.type
+// listElement.onclick = function (event){
+//   if (event.target.dataset.index){
+//     const index = parseInt(event.target.dataset.index)
+//     const type = event.target.dataset.type
      
-    if(type === "toogle"){
-      notes[index].complited = !notes[index].complited
+//     if(type === "toogle"){
+//       notes[index].complited = !notes[index].complited
 
      
-    } else if (type === "remove"){
-      notes.splice(index, 1)
+//     } else if (type === "remove"){
+//       notes.splice(index, 1)
 
 
-    }
-    render();
+//     }
+//     render();
 
-  }
+//   }
   
-  // console.log((event.target.dataset.index), (event.target.dataset.type) )
-};
+//   // console.log((event.target.dataset.index), (event.target.dataset.type) )
+// };
 
-function getNoteTemplate(note, index) {
-  return `
-    <li
-      class="list-group-item d-flex justify-content-between align-items-center"
-      >
-        <span 
-        class="${note.complited ? 'text-decoration-line-through' : '' }">${note.title}
-        </span>
-      <span>
-          <span 
-          class="btn btn-small 
-          btn-${note.complited ? 'warning' : 'success'}" 
-          data-index = "${index}" 
-          data-type="toogle"
-          >&check;
-          </span>
+// function getNoteTemplate(note, index) {
+//   return `
+//     <li
+//       class="list-group-item d-flex justify-content-between align-items-center"
+//       >
+//         <span 
+//         class="${note.complited ? 'text-decoration-line-through' : '' }">${note.title}
+//         </span>
+//       <span>
+//           <span 
+//           class="btn btn-small 
+//           btn-${note.complited ? 'warning' : 'success'}" 
+//           data-index = "${index}" 
+//           data-type="toogle"
+//           >&check;
+//           </span>
 
-          <span 
-          class="btn btn-small btn-danger"
-          data-index = "${index}" 
-          data-type="remove"
-          >&times;
-          </span>
-      </span>
-    </li>
-  `
-}
+//           <span 
+//           class="btn btn-small btn-danger"
+//           data-index = "${index}" 
+//           data-type="remove"
+//           >&times;
+//           </span>
+//       </span>
+//     </li>
+//   `
+// }
 
 
 
---
+++ practic2/array-theory.js
@@ -0,0 +1,28 @@
+
+
+
+
+
+const names = ['Ноль',  'Один', 'Два','Тры']  
+
+// names.push('Алена')
+// names.unshift('Алена')
+// const firstName = names.shift()
+// const firstName = names.pop()
+// console.log(firstName)
+// console.log('Names: ', names)
+
+// console.log(names.toSorted()) 
+
+// console.log('Names: ', names)
+// console.log(names.reverse()) 
+
+// console.log(names.toSpliced(0,1))
+
+console.log('Names: ', names);
+
+const dva = 'Два';
+
+index = names.indexOf(dva);
+
+console.log(index)
