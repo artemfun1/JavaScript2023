e9bde00198436fbbb57a45bffd566be92a2d6794
ArtemPeshkov <a.o.peshkov@yandex.ru>
2023-08-08 23:08:49 +0500

    complite array

--- practic2/array-theory.js
+++ practic2/array-theory.js
@@ -3,7 +3,7 @@
 
 
 
-const names = ['Ноль',  'Один', 'Два','Тры']  
+// const names = ['Ноль',  'Один', 'Два','Тры']  
 
 // names.push('Алена')
 // names.unshift('Алена')
@@ -15,14 +15,106 @@ const names = ['Ноль',  'Один', 'Два','Тры']
 // console.log(names.toSorted()) 
 
 // console.log('Names: ', names)
-// console.log(names.reverse()) 
+// console.log(naxmes.reverse()) 
 
 // console.log(names.toSpliced(0,1))
 
-console.log('Names: ', names);
 
-const dva = 'Два';
+// const names = ['Ноль',  'Один', 'Два','Тры'];
 
-index = names.indexOf(dva);
+// console.log('Names: ', names);
 
-console.log(index)
+// const dva = 'Два';
+
+// index = names.indexOf(dva);
+
+// console.log(index);
+
+// console.log(names[index]);
+// names[index] = ' nolnol';
+// console.log('Names: ', names);
+// const namess = names.with(index, 'olololo')
+// console.log(names)
+// console.log(namess)
+
+// const capitalName =  names.map( function(name, index){
+//   if (index === 2){
+//     return 'ololo'
+//   }
+//   return name
+// })  
+
+// console.log(names);
+// console.log(capitalName);
+
+// console.log(names.includes('Два'))
+
+
+
+
+// console.log(people.indexOf({budget: 300}))
+// let finddedPerson
+// for (let person of people){
+//   if (person.budget === 7520){
+//     finddedPerson = person
+//   }
+// }
+// console.log(finddedPerson)
+
+
+// const finded = people.find(
+//   (p) => p.budget  === 7520
+
+// )
+
+
+// const finded = people.findIndex( function(person){
+//     return person.budget === 7520
+// })
+
+// console.log(people.copyWithin())
+
+
+
+
+
+
+// const filtered = people.filter(function(p){
+//   return p.budget > 5000;
+
+// })
+
+// console.log(people)
+
+// console.log(filtered)
+
+// filtered.forEach( function(p){
+//   sumBudget += p.budget
+// })
+
+// const people = [ 
+//   {name: 'Vladilen', budget: 4200},
+//   {name: 'Elena', budget: 15200},
+//   {name: 'Igor', budget: 300},
+//   {name: 'Ksenia', budget: 7520}
+// ]
+
+// let sumBudget = people.filter((p) => p.budget > 5000).map((p) => p.budget).reduce((acc, p ) => acc + p, 0 )
+
+
+// console.log(sumBudget)
+
+
+// const string = 'Привет, как дела?'
+
+// const reversed = string
+// .split('')
+// .toReversed()
+// .join('!')
+// .split('')
+// .filter((c)=> c !== "!")
+// .join('')
+
+
+
+// console.log(reversed)
