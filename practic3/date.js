9fa5fe1e9421d0de3e922f4a2660a2f25ca7ccc3
ArtemPeshkov <a.o.peshkov@yandex.ru>
2023-08-17 00:44:37 +0500

    some stydy time/date

--- practic3/date.js
+++ practic3/date.js
@@ -165,10 +165,6 @@
 //   title: "My menu"
 // };
 
-
-
-
-
 // Напишите функцию getNumberDigit(), которая принимает число (number) и индекс цифры в числе (digitPosition). Возвращает она цифру числа number, находящуюся на позиции digitPosition.
 
 // Если происходит попытка чтения числа на позиции, которой не существует (например, кол-во цифр в числе меньше, чем передано в digitPosition), то верните undefined.
@@ -179,18 +175,16 @@
 //   // Ваш код здесь...
 // }
 
-
-
 // function getNumberDigit(number, digitPosition) {
-  
+
 //   let result = [];
-  
+
 // do {
 //     result.push(number % 10);
 //     number = Math.floor(number / 10);
 //   } while (number != 0);
 //   result.reverse()
-  
+
 //   if(typeof result[digitPosition] === 'number'){
 //   return console.log(result[digitPosition])
 // }else{
@@ -203,4 +197,60 @@
 // getNumberDigit(123, 2) // 3
 // getNumberDigit(1, 2) // undefined
 
+// const now = new Date();
 
+// const start = new Date(1000 * 60 * 60 * 24 * 365 )
+
+// const date = new Date(2011, 0, 4, 12, 42, 12)
+
+// console.log(now.getFullYear())
+// console.log((now.getMonth()+1))
+// console.log(now.getDate())
+// console.log(now.setHours(20))
+// console.log(now.getMinutes())
+// console.log(now.getSeconds())
+// console.log(now.getMilliseconds())
+
+// console.log(now.toDateString())
+// console.log(now.toTimeString())
+// console.log(now.toLocaleDateString())
+// console.log(now.toLocaleTimeString())
+
+// ========
+
+let mode = 'full'
+const output = document.getElementById('output');
+const fullBtn = document.getElementById("full");
+const dateBtn = document.getElementById("date");
+const timeBtn = document.getElementById("time"); 
+
+// const now = new Date()
+function bindMode(name) {
+  return function(){
+    mode = name
+    update()
+  }
+}
+
+fullBtn.onclick = bindMode('full')
+dateBtn.onclick = bindMode("date")
+timeBtn.onclick = bindMode("time")
+
+setInterval(update, 1000)
+update()
+
+function update(){
+  output.textContent = format(mode)
+}
+
+function format(formatMode){
+  const now = new Date()
+
+  switch(formatMode){
+      case 'time' : return now.toLocaleTimeString() +" "+ now.getMilliseconds()
+      case 'date' : return now.toLocaleDateString()
+      case 'full' : return now.toLocaleDateString() +
+  "   " + now.toLocaleTimeString ()
+      default: return now.toLocaleTimeString()
+}
+}
