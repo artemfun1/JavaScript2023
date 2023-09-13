b047481fe7be09bff6ea39877e27d92c437288d2
ArtemPeshkov <a.o.peshkov@yandex.ru>
2023-09-13 22:57:19 +0500

    final video curs

---
+++ apps.js
@@ -0,0 +1,56 @@
+// const car = {
+//   model: "tesla",
+//   year: 2023,
+// }
+
+// const json = JSON.stringify(car)
+// console.log(json)
+
+// const parst = JSON.parse(json)
+// console.log(parst)
+
+// const list = document.querySelector('#list')
+// const filter = document.querySelector('#filter')
+// let USERS = []
+
+// filter.addEventListener('input', (event)=>{
+//   const value = event.target.value.toLowerCase()
+
+//   const filteredUsers = USERS.filter((user)=>{
+//     return user.name.toLowerCase().includes(value)
+//   })
+//   render(filteredUsers)
+// })
+
+// async function start() {
+//   list.innerHTML = 'Loading...'
+//   try{
+//     const resp = await fetch('https://jsonplaceholder.typicode.com/users')
+//     const data = await resp.json()
+//     setTimeout(()=>{
+//       USERS = data
+//       render(data)
+//     },2000)
+    
+//   } catch(err) {
+//     list.style.color = 'red'
+//     list.innerHTML = err.message
+//   }
+// }
+// function render(users = []){
+//   if(users.length === 0){
+//     list.innerHTML = 'NO matched users!'
+//   } else{
+//     const html = users.map(toHTML).join('')
+// list.innerHTML = html
+//   }
+
+// }
+
+// function toHTML(user){
+//   return `
+//   <li class="list-group-item>${user.name}</li>
+//   `
+// }
+
+// start()
